e7948ef233d3b41f46406a00eb4733b32263aaac
Wybe Westra <dev@wwestra.nl>
2023-03-19 15:48:40 +0100

    Can reliably detect the "pompebled" stamp

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,24 +1,30 @@
 extends CenterContainer
 
+class_name MainScene
+
 const INCH_TO_CM := 2.54
+const POMPEBLED := Vector3(2.4, 2.9, 1.4)
+
+# How far off are distances allowed to be to still be considered to match.
+const MATCH_TOLERANCE_CM := 0.3
 
 var touches = {}
 
 # How many dots (for now we assume that is pixels) per centimeter on the screen.
 # Is valid after the `_ready()` function of this node has run.
-var dots_per_centimeter := 0
+var dots_per_centimeter: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var screen_id := DisplayServer.window_get_current_screen()
 	var screen_dpi := DisplayServer.screen_get_dpi(screen_id)
-	dots_per_centimeter = screen_dpi / INCH_TO_CM
+	dots_per_centimeter = float(screen_dpi) / INCH_TO_CM
 	
 	print("dots per cm: %s" % dots_per_centimeter)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _unhandled_input(event: InputEvent):
@@ -37,27 +43,71 @@ func _unhandled_input(event: InputEvent):
 func process_touches():
 	if touches.size() == 3:
 		var values := touches.values()
+		$Label.text = "%s\n%s\n%s\n" % [values[0]/dots_per_centimeter, values[1]/dots_per_centimeter, values[2]/dots_per_centimeter]
+		
 		var distances := calculate_distances_clockwise(values[0], values[1], values[2])
-		$Label.text = str(distances)
+		var distances_cm := distances / dots_per_centimeter
+		$Label.text += "(%.1f, %.1f, %.1f)" % [distances_cm.x, distances_cm.y, distances_cm.z]
+		
+		if distances_match_template(distances_cm, POMPEBLED):
+			$Label.text += "\nPompebled"
 
-# Calculates the distances between three points in cm, and returns them in
+# Calculates the distances between three points in pixels, and returns them in
 # clockwise order, regardless of the original order of the points.
-func calculate_distances_clockwise(p0: Vector2, p1: Vector2, p2: Vector2) -> Vector3:
-	var angle := p0.angle_to(p1)
-	var distances := Vector3.ZERO
+static func calculate_distances_clockwise(p0: Vector2, p1: Vector2, p2: Vector2) -> Vector3:
+	var dist := Vector3.ZERO
 	
-	if angle <= 0:
-		# Points are in clockwise order
-		distances.x = p0.distance_to(p1)
-		distances.y = p1.distance_to(p2)
-		distances.z = p2.distance_to(p0)
+	# Not the shortest way of doing this, but it works.
+	if p0.x < p1.x and p0.x < p2.x:
+		# p0 is leftmost point
+		if p1.y > p2.y:
+			dist.x = p0.distance_to(p1)
+			dist.y = p1.distance_to(p2)
+			dist.z = p2.distance_to(p0)
+		else:
+			dist.x = p0.distance_to(p2)
+			dist.y = p2.distance_to(p1)
+			dist.z = p1.distance_to(p0)
+	elif p1.x < p0.x and p1.x < p2.x:
+		# p1 is leftmost point.
+		if p0.y > p2.y:
+			dist.x = p1.distance_to(p0)
+			dist.y = p0.distance_to(p2)
+			dist.z = p2.distance_to(p1)
+		else:
+			dist.x = p1.distance_to(p2)
+			dist.y = p2.distance_to(p0)
+			dist.z = p0.distance_to(p1)
 	else:
-		# Points are in counterclockwise order
-		distances.x = p0.distance_to(p2)
-		distances.y = p2.distance_to(p1)
-		distances.z = p1.distance_to(p0)
+		# p2 is leftmost point.
+		if p0.y > p1.y:
+			dist.x = p2.distance_to(p0)
+			dist.y = p0.distance_to(p1)
+			dist.z = p1.distance_to(p2)
+		else:
+			dist.x = p2.distance_to(p1)
+			dist.y = p1.distance_to(p0)
+			dist.z = p0.distance_to(p2)
 	
-	# Scale so we return the distances in centimeters.
-	distances /= dots_per_centimeter
+	return dist
+
+
+# Returns true if the given distances are within tolerances to the template
+# and in the same order.
+# Expects both distances and template to be listed clockwise.
+func distances_match_template(distances: Vector3, template: Vector3) -> bool:
+	if within_tolerance(distances.x, template.x) and within_tolerance(distances.y, template.y) and within_tolerance(distances.z, template.z):
+		return true
+	elif within_tolerance(distances.y, template.x) and within_tolerance(distances.z, template.y) and within_tolerance(distances.x, template.z):
+		return true
+	elif within_tolerance(distances.z, template.x) and within_tolerance(distances.x, template.y) and within_tolerance(distances.y, template.z):
+		return true
+	else:
+		return false
+
+# Checks if the value is within `MATCH_TOLERANCE` from the target.
+func within_tolerance(value: float, target: float) -> bool:
+	var lower := target - MATCH_TOLERANCE_CM
+	var upper := target + MATCH_TOLERANCE_CM
 	
-	return distances
+	return value >= lower and value <= upper
---
+++ tests/test_calculations.gd
@@ -0,0 +1,16 @@
+extends GutTest
+
+
+var distance_parameters = [
+	[Vector2(0., 0.), Vector2(1., 0.), Vector2(1., 1.), Vector3(1.414, 1, 1)],
+	[Vector2(1., 1.), Vector2(0., 0.), Vector2(1., 0.), Vector3(1.414, 1, 1)],
+	[Vector2(1., 0.), Vector2(1., 1.), Vector2(0., 0.), Vector3(1.414, 1, 1)],
+	[Vector2(0., 0.), Vector2(1., 1.), Vector2(1., 0.), Vector3(1.414, 1, 1)],
+	[Vector2(1., 0.), Vector2(0., 0.), Vector2(1., 1.), Vector3(1.414, 1, 1)],
+	[Vector2(1., 1.), Vector2(1., 0.), Vector2(0., 0.), Vector3(1.414, 1, 1)],
+	[Vector2(18.43, 3.25), Vector2(18.33, 7.25), Vector2(16, 7.3), Vector3(2.33, 4., 4.723)],
+	[Vector2(9.7, 4.5), Vector2(11.1, 4.3), Vector2(11., 1.9), Vector3(1.41, 2.40, 2.906)],
+	[Vector2(11.5, 4.3), Vector2(11.33, 1.9), Vector2(10.15, 4.5), Vector3(1.36, 2.41, 2.85)],
+]
+func test_distance_calculation(p = use_parameters(distance_parameters)):
+	assert_almost_eq(MainScene.calculate_distances_clockwise(p[0], p[1], p[2]), p[3], Vector3(0.01, 0.01, 0.01))
